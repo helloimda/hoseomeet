aa0a5047146982f906bd7e613b30451d9150ef9c
minseok <149202694+helloimda@users.noreply.github.com>
2024-10-22 17:26:08 +0900

    Fix: 웹소켓 오류 해결

--- lib/api/chat/socket_message_service.dart
+++ lib/api/chat/socket_message_service.dart
@@ -3,7 +3,10 @@ import 'dart:convert';
 import 'dart:io';
 
 class SocketMessageService {
-  final String socketUrl = 'ws://127.0.0.1:8000/api/v1/events/connect';
+  //final String socketUrl = 'ws://127.0.0.1:8000/api/v1/events/connect';
+   final String socketUrl = 'ws://10.0.2.2:8000/api/v1/events/connect'; //에뮬레이터는 해당 호스트를 사용해야함
+ //final String socketUrl = 'ws:/www.campus-meet.shop/api/v1/events/connect'; //실제 릴리즈앱은 도메인 사용해야함
+   // 배포시 config.dart와 연동후 사용
   final String _token;
 
   WebSocket? _socket;
@@ -15,21 +18,34 @@ class SocketMessageService {
   }
 
   Future<void> connectWebSocket() async {
-    try {
-      _socket = await WebSocket.connect(
-        socketUrl,
-        headers: {
-          'sec-websocket-protocol': _token,
-        },
-      );
-      print('WebSocket 연결 성공');
-      _socket!.listen(
-        _onMessageReceived,
-        onDone: _onSocketDone,
-        onError: _onSocketError,
-      );
-    } catch (error) {
-      print('WebSocket 연결 실패: $error');
+    int retryCount = 0;
+    const maxRetries = 3;
+
+    while (retryCount < maxRetries) {
+      print("현재 토큰값은 ${_token}입니다. 웹소켓 시작");
+      try {
+        _socket = await WebSocket.connect(
+          socketUrl,
+          headers: {
+            'sec-websocket-protocol': _token,
+          },
+        );
+        print('WebSocket 연결 성공');
+        _socket!.listen(
+          _onMessageReceived,
+          onDone: _onSocketDone,
+          onError: _onSocketError,
+        );
+        break;
+      } catch (error) {
+        retryCount++;
+        print('WebSocket 연결 실패: $error. 재시도 중... ($retryCount/$maxRetries)');
+        await Future.delayed(Duration(seconds: 2)); // 2초 후에 다시 시도
+      }
+    }
+
+    if (_socket == null) {
+      print('WebSocket 연결 실패: 재시도 한도를 초과했습니다.');
     }
   }
 
